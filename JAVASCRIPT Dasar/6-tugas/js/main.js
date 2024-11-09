95787a48facf35215d4dbbd0c0bf64747c837b6c
JourneyAries <journeyaries13@gmail.com>
2024-11-10 05:56:56 +0700

    [Function][100%] JS/Part-4/6

--- JAVASCRIPT Dasar/6-tugas/js/main.js
+++ JAVASCRIPT Dasar/6-tugas/js/main.js
@@ -1,3 +1,8 @@
-//
-//
+//6. Hitung Poin Skor Game
+//Buat fungsi calculateScore(pointsArray) yang menerima array angka dan mengembalikan jumlah total poin yang dihitung dari array tersebut.
 
+function calculateScore(score) {
+  return score.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+
+console.log(calculateScore([2,3,4]));

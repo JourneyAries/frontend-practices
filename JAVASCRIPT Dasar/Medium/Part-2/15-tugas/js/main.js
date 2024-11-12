33a31149d4126299bf62f8c769962d4d66dd9741
JourneyAries <journeyaries13@gmail.com>
2024-11-12 14:36:41 +0700

    [Function][100%] JS/Medium/Part-2/15

---
+++ JAVASCRIPT Dasar/Medium/Part-2/15-tugas/js/main.js
@@ -0,0 +1,8 @@
+//15. Menemukan Nilai yang Berada pada Posisi Tertentu
+//Buat fungsi findAtIndex(arr, index) yang menerima array arr dan indeks index, dan mengembalikan nilai yang berada pada indeks tersebut.
+
+function findAtIndex(arr, index) {
+  return arr[index];
+}
+
+console.log(findAtIndex([10, 20, 30], 1));

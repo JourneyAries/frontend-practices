c75d4fb2a900dbfd30f8af3618c7bc1d5a2eeea2
JourneyAries <journeyaries13@gmail.com>
2024-11-16 18:21:52 +0700

    [Function][100%] JS/Easy/Part-2/8

---
+++ JAVASCRIPT Dasar/Easy/Part-2/8-tugas/js/main.js
@@ -0,0 +1,36 @@
+//8. Mengembalikan String dengan Huruf Kapital di Awal
+//Fungsi: capitalizeFirstLetter(str)
+//Parameter: str (string)
+//Hint: Ubah huruf pertama dengan .toUpperCase().
+//Contoh Pemanggilan: capitalizeFirstLetter("hello") // "Hello"
+
+//charAt
+const capitalizeFirstLetter1 = str => {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+}
+
+//substring
+const capitalizeFirstLetter2 = str => {
+  return str.substring(0,1).toUpperCase() + str.substring(1).toLowerCase();
+}
+
+//split, map, join
+const capitalizeFirstLetter3 = str => {
+  return str
+    .split('')
+    .map((char, index) => index === 0 ? char.toUpperCase() : char.toLowerCase())
+    .join('');
+}
+
+//regex replace
+const capitalizeFirstLetter4 = str => {
+    return str.replace(/^(\w)(.*)/, (match, firstChar, rest) => {
+    return firstChar.toUpperCase() + rest.toLowerCase();
+  });
+}
+
+
+console.log(capitalizeFirstLetter1("hello"));
+console.log(capitalizeFirstLetter2("hello"));
+console.log(capitalizeFirstLetter3("hello"));
+console.log(capitalizeFirstLetter4("hello"));

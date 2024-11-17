79dd26a056791d242108ffeb244c77934a4e7afe
JourneyAries <journeyaries13@gmail.com>
2024-11-17 16:52:45 +0700

    [Function][100%] JS/Easy/Part-2/20

---
+++ JAVASCRIPT Dasar/Easy/Part-2/20-tugas/js/main.js
@@ -0,0 +1,27 @@
+//20. Konversi Angka Menjadi Array Digit
+//Fungsi: numberToArray(num)
+//Parameter: num (integer)
+//Hint: Ubah angka ke string, lalu split dan konversi ke angka.
+//Contoh Pemanggilan: numberToArray(123) // [1, 2, 3]
+
+//cara pertama
+function numberToArray1(num) {
+  return num.toString().split('').map(Number);
+}
+
+//cara kedua
+function numberToArray2(num){
+  return Array.from(String(num), Number);
+}
+
+//cara ketiga
+function numberToArray3(num) {
+  return [...num.toString()].map(Number);
+}
+
+
+
+
+console.log(numberToArray1(123));
+console.log(numberToArray2(123));
+console.log(numberToArray3(123));

405869a0a4f182f9f052015ca07e120601964e29
JourneyAries <journeyaries13@gmail.com>
2024-11-17 19:54:22 +0700

    [Function][100%] JS/Easy/Part-2/23

--- JAVASCRIPT Dasar/Easy/Part-2/23-tugas/js/main.js
+++ JAVASCRIPT Dasar/Easy/Part-2/23-tugas/js/main.js
@@ -1,6 +1,24 @@
-//
-//
-//
-//
-//
+//23. Mengganti Karakter dalam String
+//Fungsi: replaceChar(str, target, replacement)
+//Parameter: str (string), target (char), replacement (char)
+//Hint: Gunakan metode .replaceAll.
+//Contoh Pemanggilan: replaceChar("hello", "l", "z") // "hezzo"
 
+//replaceAll
+function replace1(str, target, replacement) {
+  return str.replaceAll(target, replacement);
+}
+
+//split join
+function replace2(str, target, replacement) {
+  return str.split(target).join(replacement);
+}
+
+//replace regExp
+function replace3(str, target, replacement) {
+  return str.replace(new RegExp(target, 'g'), replacement)
+}
+
+console.log(replace1("hello", "l", "z"));
+console.log(replace2("hello", "l", "z"));
+console.log(replace3("hello", "l", "z"));

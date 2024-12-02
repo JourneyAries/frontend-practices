582c67e0c9bd52b0ea2d80bcb6561c4897a8a541
JourneyAries <journeyaries13@gmail.com>
2024-12-02 13:17:15 +0700

    JS/Hard/Part-1/3-tugas

---
+++ JAVASCRIPT Dasar/Hard/Part-1/3-tugas/js/main.js
@@ -0,0 +1,6 @@
+// Tugas 3: DOM Selection - Pilih Elemen dengan Query Selector
+// Soal: Ubah warna teks elemen dengan kelas highlight menjadi merah.
+// Hint: Gunakan document.querySelector('.highlight').
+
+const highlight = document.querySelector('.highlight');
+highlight.style.color = 'red';

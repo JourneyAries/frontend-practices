200193ae0ea9b1ebc5ab3042885af49d0acccc48
JourneyAries <journeyaries13@gmail.com>
2024-12-02 13:04:14 +0700

    QQQQQJS Dasar/Easy/Re-learn/Part-2/2-tugas
    git commit -m

---
+++ JAVASCRIPT Dasar/Hard/Part-1/1-tugas/js/main.js
@@ -0,0 +1,6 @@
+// Tugas 1: Pendahuluan DOM - Ubah Konten Teks
+// Soal: Ubah teks elemen < p > dengan id intro menjadi "Hello, DOM!".
+// Hint: Gunakan document.getElementById('intro') dan ubah properti .innerText.
+
+const pIntro = document.getElementById('intro');
+pIntro.innerHTML = ('Hello, DOM!');

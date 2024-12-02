872b53ce65a3570107bf88f9223015e2685a13f8
JourneyAries <journeyaries13@gmail.com>
2024-12-02 13:43:13 +0700

    JS/Hard/Part-1/6-tugas

---
+++ JAVASCRIPT Dasar/Hard/Part-1/6-tugas/js/main.js
@@ -0,0 +1,6 @@
+// Tugas 6: DOM Traversal - Pilih Elemen Parent
+// Soal: Pilih elemen parent dari elemen dengan id child dan ubah warna background - nya menjadi abu - abu.
+// Hint: Gunakan .parentElement untuk mendapatkan elemen parent.
+
+const child = document.getElementById('child');
+child.parentElement.style.backgroundColor = 'gray';

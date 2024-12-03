d6e0bc479e5182df4e497263f38ec2760dea5215
JourneyAries <journeyaries13@gmail.com>
2024-12-03 14:15:12 +0700

    JS/Hard/Part-2/3-tugas

---
+++ JAVASCRIPT Dasar/Hard/Part-2/3-tugas/js/main.js
@@ -0,0 +1,8 @@
+// Soal 3: Pilih Elemen Berdasarkan Tag
+// Soal: Pilih semua elemen <p> dan ubah teksnya menjadi "Paragraph updated".
+// Hint: Gunakan document.getElementsByTagName().
+
+const paragraphs = document.getElementsByTagName('p');
+for (let paragraph of paragraphs) {
+	paragraph.textContent = 'Paragraph Updated';
+}

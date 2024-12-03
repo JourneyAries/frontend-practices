ff13da12a73c13051962c20221bd15bcf5c78851
JourneyAries <journeyaries13@gmail.com>
2024-12-03 14:45:12 +0700

    JS/Hard/Part-2/5-tugas

---
+++ JAVASCRIPT Dasar/Hard/Part-2/5-tugas/js/main.js
@@ -0,0 +1,6 @@
+// Soal 5: Pilih Elemen yang Memiliki Kelas dan ID
+// Soal: Pilih elemen yang memiliki kelas box dan ID special dan ubah warna latarnya menjadi kuning.
+// Hint: Gunakan document.querySelector().
+
+const box = document.querySelector('.box#special');
+box.style.backgroundColor = 'yellow';

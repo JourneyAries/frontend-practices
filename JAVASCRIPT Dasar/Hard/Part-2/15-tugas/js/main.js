1298b2dfa51d66dd4f4d96f177c2dd7609490dda
JourneyAries <journeyaries13@gmail.com>
2024-12-04 13:38:28 +0700

    JS/Hard/Part-2/15-tugas

---
+++ JAVASCRIPT Dasar/Hard/Part-2/15-tugas/js/main.js
@@ -0,0 +1,5 @@
+// Soal 15: Replace Element
+// Soal: Ganti teks dari elemen pertama dalam daftar <ul> dengan "Replaced Item".
+// Hint: Gunakan firstElementChild dan textContent.
+
+const firstElementChild = (document.getElementById('list').firstElementChild.textContent = 'Replaced Item');

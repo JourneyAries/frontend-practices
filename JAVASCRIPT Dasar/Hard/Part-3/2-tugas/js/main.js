896f12541a6729aad7dbafe1d3e95d910e10665b
JourneyAries <journeyaries13@gmail.com>
2024-12-05 13:46:52 +0700

    JS/Hard/Part-3/1-tugas v2.5

--- JAVASCRIPT Dasar/Hard/Part-3/2-tugas/js/main.js
+++ JAVASCRIPT Dasar/Hard/Part-3/2-tugas/js/main.js
@@ -8,15 +8,35 @@
 // v3: Gunakan dropdown untuk memilih operasi matematika.
 
 const calculator = document.getElementById('calculator');
+
+// const operations = [
+// 	{ id: 'tambah', label: 'Tambah', action: 'tambah' },
+// 	{ id: 'kurang', label: 'Kurang', action: 'kurang' },
+// 	{ id: 'kali', label: 'Kali', action: 'kali' },
+// 	{ id: 'bagi', label: 'Bagi', action: 'bagi' },
+// ];
+
+// operations.forEach(({ id, label, action }) => {
+//   const button = document.createElement('button');
+//   button.id = id;
+//   button.textContent = label;
+//   calculator.appendChild(button);
+// })
+
 const btnTambah = document.getElementById('tambah');
 const btnKurang = document.getElementById('kurang');
 const btnKali = document.getElementById('kali');
 const btnBagi = document.getElementById('bagi');
 const result = document.getElementById('result');
 
+const getInputs = () => {
+	const input1 = parseFloat(document.getElementById('num1').value);
+	const input2 = parseFloat(document.getElementById('num2').value);
+	return { input1, input2 };
+};
+
 const calculate = (operation) => {
-	const input1 = parseInt(document.getElementById('num1').value);
-	const input2 = parseInt(document.getElementById('num2').value);
+	const { input1, input2 } = getInputs();
 	let total;
 
 	if (isNaN(input1) || isNaN(input2)) {
@@ -35,16 +55,19 @@ const calculate = (operation) => {
 			total = input1 * input2;
 			break;
 		case 'bagi':
-			total = input1 / input2;
+			if (input2 === 0) {
+				total = 'Tidak bisa dibagi dengan 0';
+			} else {
+				total = input1 / input2;
+			}
 			break;
 		default:
 			total = 'Operasi tidak dikenali';
 	}
-
-	result.textContent = `result: ${total}`;
+	result.textContent = `Result: ${total}`;
 };
 
 btnTambah.addEventListener('click', () => calculate('tambah'));
 btnKurang.addEventListener('click', () => calculate('kurang'));
-btnBagi.addEventListener('click', () => calculate('kali'));
-btnKali.addEventListener('click', () => calculate('bagi'));
+btnKali.addEventListener('click', () => calculate('kali'));
+btnBagi.addEventListener('click', () => calculate('bagi'));

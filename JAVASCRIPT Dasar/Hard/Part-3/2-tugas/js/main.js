672830834b3687edf94811e1a489def25ea6b301
JourneyAries <journeyaries13@gmail.com>
2024-12-05 08:25:33 +0700

    JS/Hard/Part-3/1-tugas v2

--- JAVASCRIPT Dasar/Hard/Part-3/2-tugas/js/main.js
+++ JAVASCRIPT Dasar/Hard/Part-3/2-tugas/js/main.js
@@ -1,66 +1,50 @@
-// Soal 1: Tebak Angka
-// Program: Buat program untuk menebak angka antara 1 - 10. Input angka, klik tombol "Cek", lalu tampilkan hasilnya(benar / salah).
-// Hint:
-// Gunakan Math.random() untuk menghasilkan angka acak antara 1-10.
-// Bandingkan input pengguna dengan angka acak.
+// 1. Kalkulator Sederhana
 // Pseudocode:
-// Buat angka acak antara 1-10.
-// Tangkap input dari pengguna saat tombol "Cek" diklik.
-// Jika angka cocok, tampilkan pesan "Benar!" di <p id="result">.
-// Jika tidak cocok, tampilkan pesan "Salah! Coba lagi."
+// Ambil nilai dari kedua input (num1, num2) saat tombol Calculate diklik.
+// Hitung penjumlahan dari kedua nilai tersebut.
+// Tampilkan hasil di dalam elemen <p> dengan ID result.
+// Upgrade (v2, v3):
+// v2: Tambahkan operasi matematika lain seperti pengurangan, perkalian, dan pembagian.
+// v3: Gunakan dropdown untuk memilih operasi matematika.
 
-const guessInput = document.getElementById('guessInput');
-const checkBtn = document.getElementById('checkBtn');
-const resetBtn = document.getElementById('resetBtn');
+const calculator = document.getElementById('calculator');
+const btnTambah = document.getElementById('tambah');
+const btnKurang = document.getElementById('kurang');
+const btnKali = document.getElementById('kali');
+const btnBagi = document.getElementById('bagi');
 const result = document.getElementById('result');
-const livesTxt = document.getElementById('lives');
 
-//random number 1-10
-const getRandomNumber = () => {
-	return Math.floor(Math.random() * 10) + 1;
-};
+const calculate = (operation) => {
+	const input1 = parseInt(document.getElementById('num1').value);
+	const input2 = parseInt(document.getElementById('num2').value);
+	let total;
 
-//start
-let randomNumber = getRandomNumber();
-let lives = 3;
-livesTxt.textContent = lives;
-console.log(randomNumber);
-
-//Listener - checkBtn
-checkBtn.addEventListener('click', () => {
-	const userGuess = parseInt(guessInput.value);
-	//validation
-	if (isNaN(userGuess) || userGuess > 10 || userGuess < 1) {
-		result.textContent = 'Masukkan angka valid antara 1 dan 10';
-		result.style.color = 'orange';
+	if (isNaN(input1) || isNaN(input2)) {
+		result.textContent = 'Masukan angka yang valid';
 		return;
 	}
 
-	//rules
-	if (userGuess === randomNumber) {
-		result.textContent = 'BENAR';
-		result.style.color = 'green';
-	} else if (userGuess > randomNumber) {
-		result.textContent = 'Tebakan terlalu TINGGI';
-		result.style.color = 'red';
-	} else {
-		result.textContent = 'Tebakan terlalu RENDAH';
-		result.style.color = 'red';
+	switch (operation) {
+		case 'tambah':
+			total = input1 + input2;
+			break;
+		case 'kurang':
+			total = input1 - input2;
+			break;
+		case 'kali':
+			total = input1 * input2;
+			break;
+		case 'bagi':
+			total = input1 / input2;
+			break;
+		default:
+			total = 'Operasi tidak dikenali';
 	}
 
-	lives--;
-	livesTxt.textContent = lives;
+	result.textContent = `result: ${total}`;
+};
 
-	if (lives === 0) {
-		result.textContent = `Selesai! Angka yang benar adalah ${randomNumber}.`;
-		result.style.color = 'purple';
-	}
-});
-
-//Listener - resetBtn
-resetBtn.addEventListener('click', () => {
-	randomNumber = getRandomNumber();
-	guessInput.value = '';
-	result.textContent = '';
-	lives = 3;
-});
+btnTambah.addEventListener('click', () => calculate('tambah'));
+btnKurang.addEventListener('click', () => calculate('kurang'));
+btnBagi.addEventListener('click', () => calculate('kali'));
+btnKali.addEventListener('click', () => calculate('bagi'));

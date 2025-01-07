f7eaafb8e41c65fcc535dfe856f2805b16df51da
JourneyAries <journeyaries13@gmail.com>
2025-01-07 08:17:37 +0700

    JS Lanjutan/Easy/Part-2/4-5

--- JAVASCRIPT Lanjutan/Easy/Part-2/4_Destructuring Object Basic/js/main.js
+++ JAVASCRIPT Lanjutan/Easy/Part-2/4_Destructuring Object Basic/js/main.js
@@ -1,6 +1,7 @@
-// Tugas: Gunakan destructuring untuk mengambil properti dari object
+// Tugas: Gunakan rest operator untuk mengambil sisa elemen array
 
-const obj = { name: 'udin', age: 15 };
-const { name, age } = obj;
-console.log(name);
-console.log(age);
+const arr = [1, 2, 3, 4, 5, 6];
+const [a, b, ...rest] = arr;
+console.log(a);
+console.log(b);
+console.log(rest);

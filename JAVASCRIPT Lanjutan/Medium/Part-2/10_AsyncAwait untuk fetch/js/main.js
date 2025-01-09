f0da2c174677016de7db545d6fc6fddacb140835
JourneyAries <journeyaries13@gmail.com>
2025-01-09 09:51:13 +0700

    JS Lanjutan/Medium/Part-2/10

---
+++ JAVASCRIPT Lanjutan/Medium/Part-2/10_AsyncAwait untuk fetch/js/main.js
@@ -0,0 +1,9 @@
+// Tugas: Gunakan `async/await` untuk menangani operasi asinkron.
+
+async function fetchData() {
+	const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+	const data = await response.json();
+	console.log(data);
+}
+
+fetchData();

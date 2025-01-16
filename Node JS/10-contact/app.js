64c0a09fe97c80fb2a76d3cc0fb5ea474a6bd85a
JourneyAries <journeyaries13@gmail.com>
2025-01-16 14:17:20 +0700

    contact v3

--- Node JS/10-contact/app.js
+++ Node JS/10-contact/app.js
@@ -1,28 +1,68 @@
 const contacts = require('./contacts');
 const yargs = require('yargs');
 
+yargs
+	.command({
+		command: 'add',
+		describe: 'Menambahkan contact baru',
+		builder: {
+			nama: {
+				describe: 'Nama lengkap',
+				demandOption: true,
+				type: 'string',
+			},
+			email: {
+				describe: 'Email',
+				demandOption: false,
+				type: 'string',
+			},
+			noHP: {
+				describe: 'Nomor Handphone',
+				demandOption: true,
+				type: 'string',
+			},
+		},
+		handler(argv) {
+			contacts.simpanContact(argv.nama, argv.email, argv.noHP);
+		},
+	})
+	.demandCommand();
+
 yargs.command({
-	command: 'add',
-	describe: 'Menambahkan contact baru',
+	command: 'list',
+	describe: 'Menampilkan semua nama & no HP contact',
+	handler() {
+		contacts.listContact();
+	},
+});
+
+yargs.command({
+	command: 'detail',
+	describe: 'Menampilkan detail sebuah contact berdasarkan nama',
 	builder: {
 		nama: {
 			describe: 'Nama lengkap',
 			demandOption: true,
 			type: 'string',
 		},
-		email: {
-			describe: 'Email',
-			demandOption: false,
-			type: 'string',
-		},
-		noHP: {
-			describe: 'Nomor Handphone',
+	},
+	handler(argv) {
+		contacts.detailContact(argv.nama);
+	},
+});
+
+yargs.command({
+	command: 'delete',
+	describe: 'Menampilkan sebuah contact berdasarkan nama',
+	builder: {
+		nama: {
+			describe: 'Nama lengkap',
 			demandOption: true,
 			type: 'string',
 		},
 	},
 	handler(argv) {
-		contacts.simpanContact(argv.nama, argv.email, argv.noHP);
+		contacts.deleteContact(argv.nama);
 	},
 });
 
